3c80b0f6c7ddf3852aae4eea4f11ffe9d85e0306
yungtiec <tctammychu@gmail.com>
2016-08-21 22:39:41 -0400

    add google oauth

---
+++ materials.js
@@ -0,0 +1,61 @@
+var Colors = {
+    red: 0xff3c46,
+    white: 0xd8d0d1,
+    green: 0xbdf566,
+    orange: 0xF5986E,
+    pink: 0xff5d93,
+    purple: 0xa48bff,
+    blue: 0x69ceec,
+    yellow: 0xf4c637,
+    yellowDark: 0xffc72a,
+    emerald: 0x019875,
+    tangerine: 0xf05442
+};
+
+var gameoverMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.red
+});
+var redMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.red,
+    shading: THREE.FlatShading
+});
+var whiteMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.white,
+    shading: THREE.FlatShading
+});
+var greenMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.green,
+    shading: THREE.FlatShading
+});
+var purpleMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.purple,
+    shading: THREE.FlatShading
+});
+var blueMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.blue,
+    shading: THREE.FlatShading
+});
+var orangeMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.orange,
+    shading: THREE.FlatShading
+});
+var yellowMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.yellow,
+    shading: THREE.FlatShading
+});
+var yellowDarkMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.yellowDark,
+    shading: THREE.FlatShading
+});
+var pinkMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.pink,
+    shading: THREE.FlatShading
+});
+var emeraldMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.emerald,
+    shading: THREE.FlatShading
+});
+var tangerineMaterial = new THREE.MeshPhongMaterial({
+    color: Colors.tangerine,
+    shading: THREE.FlatShading
+});
